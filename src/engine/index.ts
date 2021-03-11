ca842ebbfea977592c5a6498c17c0c9a7f574c55
WhiteMinds <WhiteMind@qq.com>
2021-03-11 17:54:52 +0800

    chore: 关于战斗时的事件

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,11 +1,21 @@
 import { Subject } from 'rxjs'
 import { CombatSystem, Team } from './combat'
-import { Entity } from './model/entity'
+import { BattlingEntity, Entity } from './model/entity'
 import { EquipModule } from './module/equip'
 
 export class Engine extends EventTarget {
   entitySubjects = {
     init: new Subject<Entity>(),
+    beforeAttack: new Subject<{
+      combat: CombatSystem
+      entity: BattlingEntity
+      damage: number
+    }>(),
+    beforeDamage: new Subject<{
+      combat: CombatSystem
+      entity: BattlingEntity
+      damage: number
+    }>(),
   }
   // 需要使用 entitySubjects 的 module 应该在其下方初始化，否则会拿不到
   equipModule = new EquipModule(this)
