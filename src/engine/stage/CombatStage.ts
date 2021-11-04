77dddaa4e2fc165867175d1201735573a56f2957
WhiteMinds <WhiteMind@qq.com>
2021-11-04 15:27:59 +0800

    tmp

---
+++ src/engine/stage/CombatStage.ts
@@ -0,0 +1,26 @@
+import { DataManager, DataManager$InitWithCopy } from './DataManager'
+import { GlobalDataSource } from './MainStage'
+import { DataSource, Entity, Item, Skill } from './types'
+
+export class CombatDataSource extends EventTarget implements DataSource {
+  entities: DataManager<Entity, Entity.Serialized>
+  items: DataManager<Item, Item.Serialized>
+  skills: DataManager<Skill, Skill.Serialized>
+  // teams
+
+  constructor(private globalDS: GlobalDataSource) {
+    super()
+
+    this.entities = new DataManager$InitWithCopy(
+      Entity,
+      this,
+      this.globalDS.entities,
+    )
+    this.items = new DataManager$InitWithCopy(Item, this, this.globalDS.items)
+    this.skills = new DataManager$InitWithCopy(
+      Skill,
+      this,
+      this.globalDS.skills,
+    )
+  }
+}
---
+++ src/engine/stage/MainStage.ts
@@ -0,0 +1,31 @@
+import { EventEmitter } from 'eventemitter3'
+import { DataManager, DataManager$InitWithStore } from './DataManager'
+import { DataSource, Entity, Item, Skill } from './types'
+
+type GlobalDataSourceEventTypes = {
+  EntityCreated: [Entity]
+  ItemCreated: [Item]
+}
+
+export class GlobalDataSource
+  extends EventEmitter<GlobalDataSourceEventTypes>
+  implements DataSource {
+  // DataManager 类似于数据库中的一个表
+  entities: DataManager<Entity, Entity.Serialized>
+  items: DataManager<Item, Item.Serialized>
+  skills: DataManager<Skill, Skill.Serialized>
+  // buffs
+
+  constructor() {
+    super()
+
+    this.entities = new DataManager$InitWithStore(Entity, this)
+    this.items = new DataManager$InitWithStore(Item, this)
+    this.skills = new DataManager$InitWithStore(Skill, this)
+
+    this.entities.on('ItemAdded', (entity) =>
+      this.emit('EntityCreated', entity),
+    )
+    this.items.on('ItemAdded', (item) => this.emit('ItemCreated', item))
+  }
+}
